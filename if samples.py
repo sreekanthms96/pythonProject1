2ed15ee590fd1f2ea17c7555f3ebefdbe36a7293
Sreekanth M S <sreekusree369@gmail.com>
2024-07-05 09:35:19 +0530

    if samples edited

---
+++ if samples.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a value: "))
+b = int(input("Enter a value: "))
+
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a is equal to b")
