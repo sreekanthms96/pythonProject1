5705fc89577c3244381d7d1c70eeea6e31409479
Sreekanth M S <sreekusree369@gmail.com>
2024-07-05 16:50:18 +0530

    edited

---
+++ for loop.py
@@ -0,0 +1,7 @@
+files = ["Joker.mp4", "Memories.mp4", "Money Heist.mp4"]
+
+for x in files:  #it will print in diff line
+    x = x[0:-4]  #used to remove last 4 words
+    print(x)
+
+# print(files)
