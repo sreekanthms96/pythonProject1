369d6e8fc8f3c410bacd270ba691f1d30af0aab6
Sreekanth M S <sreekusree369@gmail.com>
2024-07-05 16:53:21 +0530

    multiplication table

--- for loop.py
+++ for loop.py
@@ -1,7 +1,10 @@
 files = ["Joker.mp4", "Memories.mp4", "Money Heist.mp4"]
 
-for x in files:  #it will print in diff line
-    x = x[0:-4]  #used to remove last 4 words
-    print(x)
+# for x in files:  #it will print in diff line
+#     x = x[0:-4]  #used to remove last 4 words
+#     print(x)
 
 # print(files)
+
+for x in range(10) :
+    print(x)
