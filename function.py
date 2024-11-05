56348f73426f89f96fde9bd9f70ee5b930a8bd38
Sreekanth M S <sreekanth.ms@cloudnautical.com>
2024-11-05 11:44:44 +0530

    functon

--- function.py
+++ function.py
@@ -1,3 +1,11 @@
-def printing():
-    print("Code working fine and Output is able to print")
-printing()
+# def printing():
+#     print("Code working fine and Output is able to print")
+# printing( )
+
+
+
+def hey(name):
+    print("My Name is " + name)
+
+Value = "Sreekanth"
+hey(Value)
