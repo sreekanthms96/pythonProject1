de1cfe00bfad9c0edd0ae18dc82b8fb63e5c0392
Sreekanth M S <sreekanth.ms@cloudnautical.com>
2024-11-05 13:02:11 +0530

    dictionary added

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+value={"name":"Sreekanth", "place":"kochi"}
+print(value) #will return entire values
+
+value={"name":"Sreekanth", "place":"kochi"}
+print(value.get("name")) #will return only values with name
