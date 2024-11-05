6dac8baba787d65bdc69d4dccdc6a5122588d0b8
Sreekanth M S <sreekanth.ms@cloudnautical.com>
2024-11-05 14:26:24 +0530

    try catch

--- length.py
+++ length.py
@@ -1,15 +1,24 @@
-a="Hi, you are doing this codes to print the total no of length of this text assigned in a"
-print(len(a))
+# a="Hi, you are doing this codes to print the total no of length of this text assigned in a"
+# print(len(a))
 
-#to find the word in the 52nd position of this array
+# #to find the word in the 52nd position of this array
 
-print(a[52])
+# print(a[52])
 
-#array is starting from position 0
+# #array is starting from position 0
 
 
 
-#to print word from 49 to 79 we use 49:79 commands
+# #to print word from 49 to 79 we use 49:79 commands
 
-print(a[49:79])
+# print(a[49:79])
 
+b=int(input("Enter a number: "))
+
+try:
+    a=10/b
+    print(a)
+    print("Try completed and value is "+str(a))
+except ZeroDivisionError:
+    print("OH... sorry Cant Divide with zero")
+print("program completed")
--- modules.py
+++ modules.py
@@ -1,2 +1,13 @@
 import ifsamples
-ifsamples.checkNegorPos(19)
+ifsamples.checkNegorPos
+
+check=ifsamples.checkNegorPos
+check(10)
+
+print(__name__) #show in which module path we are
+
+import platform
+print(platform.system())
+
+import math
+print(math.pi)
